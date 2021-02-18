d7ceaf2b2c8cd3950a77b1433aa2a68c00afc7b8
NandhiniNavaneethan251 <69723797+NandhiniNavaneethan251@users.noreply.github.com>
2021-02-18 10:06:05 +0530

    changes in html,css and typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'POC1Angular';
+  title = 'POC1Angular';  
+
+  public error = document.getElementById('error') as HTMLInputElement;
+  public Domain = document.getElementById('Domain') as HTMLInputElement;
+  public userid =document.getElementById('userid') as HTMLInputElement;
+ 
+ 
+constructor() { }
+
+ngOnInit(): void {
+}
+
+myFunc() {
+
+  var unip = ((document.getElementById("in") as HTMLInputElement).value);
+  if (unip == null || unip == "") {
+      
+    this.error.value = "Field cannot be empty";
+    this.Domain.value = "";
+    this.userid.value = "Field cannot be empty";
+  
+  }
+  
+}
 }
